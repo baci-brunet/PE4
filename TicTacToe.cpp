eb2b8189535e1b24655c72536a73305078a675c2
baci-brunet <baci@Sebastians-MBP.attlocal.net>
2021-02-16 15:07:04 -0500

    PlaceMarker commit

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int CreateBoard() {
     int board[3][3];
+    return board;
 }
 
 void DisplayBoard(int *b){
@@ -16,4 +17,13 @@ void DisplayBoard(int *b){
   std::cout<<std::endl;
 }
 
-
+void PlaceMaker(int b, int arr, int marker) {
+    int x = arr[0][0];
+    int y = arr[0][1];
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            if (i == x && j == y)
+                b[i][j] = marker;
+        }
+    }
+}
