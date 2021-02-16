c9965151793003fea5527d60fa48cbb23666ac70
baci-brunet <55216299+baci-brunet@users.noreply.github.com>
2021-02-16 12:43:42 -0700

    Adding TicTacToe.cpp with Display Board function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,3 +5,9 @@ using namespace std;
 int CreateBoard() {
     int board[3][3];
 }
+
+void DisplayBoard(int arr[3][3]) {
+    for (int i = 0; i < size(arr); i++)
+        for (int j = 0; j < size(arr[i]); j++)
+            cout << arr[i][j] << endl;
+}
